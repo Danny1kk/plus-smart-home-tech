0d863cf69aae089ed3c3f1d9d7b79e9cbb359909
Danny <danny.koshik@gmail.com>
2026-06-17 23:09:05 +0300

    fix: asynchronous grpc execution

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/TelemetryCollectorGrpcService.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/TelemetryCollectorGrpcService.java
@@ -12,6 +12,9 @@ import ru.yandex.practicum.grpc.telemetry.collector.CollectorControllerGrpc;
 import ru.yandex.practicum.grpc.telemetry.event.HubEventProto;
 import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @Slf4j
 @GrpcService
 public class TelemetryCollectorGrpcService extends CollectorControllerGrpc.CollectorControllerImplBase {
@@ -19,6 +22,8 @@ public class TelemetryCollectorGrpcService extends CollectorControllerGrpc.Colle
     private final KafkaEventProducer kafkaEventProducer;
     private final String sensorsTopic;
     private final String hubsTopic;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(4);
 
     public TelemetryCollectorGrpcService(
             KafkaEventProducer kafkaEventProducer,
@@ -32,51 +37,58 @@ public class TelemetryCollectorGrpcService extends CollectorControllerGrpc.Colle
     @Override
     public void collectSensorEvent(SensorEventProto request, StreamObserver<Empty> responseObserver) {
         log.trace("gRPC: Получено событие датчика: {}", request.getId());
-        try {
-            byte[] data = request.toByteArray();
 
-            String eventClass = request.getClass().getSimpleName().replace("Proto", "");
+        executorService.submit(() -> {
+            try {
+                byte[] data = request.toByteArray();
+                String eventClass = request.getClass().getSimpleName().replace("Proto", "");
 
-            ProducerParam param = ProducerParam.builder()
-                    .topic(sensorsTopic)
-                    .key(request.getId())
-                    .value(data)
-                    .timestamp(request.getTimestamp().getSeconds() * 1000)
-                    .eventClass(eventClass)
-                    .eventType(request.getPayloadCase().toString())
-                    .build();
+                ProducerParam param = ProducerParam.builder()
+                        .topic(sensorsTopic)
+                        .key(request.getId())
+                        .value(data)
+                        .timestamp(request.getTimestamp() != null ? request.getTimestamp().getSeconds() * 1000 : System.currentTimeMillis())
+                        .eventClass(eventClass)
+                        .eventType(request.getPayloadCase().toString())
+                        .build();
 
-            kafkaEventProducer.sendRecord(param);
-            responseObserver.onNext(Empty.getDefaultInstance());
-            responseObserver.onCompleted();
-        } catch (Exception e) {
-            log.error("Ошибка при обработке gRPC события датчика", e);
-            responseObserver.onError(Status.INTERNAL.withDescription(e.getMessage()).asException());
-        }
+                kafkaEventProducer.sendRecord(param);
+
+                responseObserver.onNext(Empty.getDefaultInstance());
+                responseObserver.onCompleted();
+            } catch (Exception e) {
+                log.error("Ошибка при асинхронной обработке gRPC события датчика", e);
+                responseObserver.onError(Status.INTERNAL.withDescription(e.getMessage()).asException());
+            }
+        });
     }
 
     @Override
     public void collectHubEvent(HubEventProto request, StreamObserver<Empty> responseObserver) {
         log.trace("gRPC: Получено событие хаба: {}", request.getHubId());
-        try {
-            byte[] data = request.toByteArray();
 
-            String eventClass = request.getClass().getSimpleName().replace("Proto", "");
+        executorService.submit(() -> {
+            try {
+                byte[] data = request.toByteArray();
+                String eventClass = request.getClass().getSimpleName().replace("Proto", "");
 
-            ProducerParam param = ProducerParam.builder()
-                    .topic(hubsTopic)
-                    .key(request.getHubId())
-                    .value(data)
-                    .timestamp(request.getTimestamp().getSeconds() * 1000)
-                    .eventClass(eventClass)
-                    .eventType(request.getPayloadCase().toString())
-                    .build();
+                ProducerParam param = ProducerParam.builder()
+                        .topic(hubsTopic)
+                        .key(request.getHubId())
+                        .value(data)
+                        .timestamp(request.getTimestamp() != null ? request.getTimestamp().getSeconds() * 1000 : System.currentTimeMillis())
+                        .eventClass(eventClass)
+                        .eventType(request.getPayloadCase().toString())
+                        .build();
 
-            kafkaEventProducer.sendRecord(param);
-            responseObserver.onNext(Empty.getDefaultInstance());
-            responseObserver.onCompleted();
-        } catch (Exception e) {
-            log.error("Ошибка при обработке события хаба: {}", request.getHubId(), e);
-            responseObserver.onError(Status.INTERNAL.withDescription(e.getMessage()).asException());        }
+                kafkaEventProducer.sendRecord(param);
+
+                responseObserver.onNext(Empty.getDefaultInstance());
+                responseObserver.onCompleted();
+            } catch (Exception e) {
+                log.error("Ошибка при асинхронной обработке события хаба: {}", request.getHubId(), e);
+                responseObserver.onError(Status.INTERNAL.withDescription(e.getMessage()).asException());
+            }
+        });
     }
 }
