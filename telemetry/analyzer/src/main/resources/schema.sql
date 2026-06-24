e6162dec16a549d14c8ee520fef370c897235ec1
Danny <danny.koshik@gmail.com>
2026-06-24 17:15:28 +0300

    fix(analyzer): update database schema with proper types and triggers

--- telemetry/analyzer/src/main/resources/schema.sql
+++ telemetry/analyzer/src/main/resources/schema.sql
@@ -1,53 +1,51 @@
 CREATE TABLE IF NOT EXISTS scenarios (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    hub_id VARCHAR,
-    name VARCHAR,
+    hub_id VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     UNIQUE(hub_id, name)
 );
 
 CREATE TABLE IF NOT EXISTS sensors (
-    id VARCHAR PRIMARY KEY,
-    hub_id VARCHAR
+    id VARCHAR(255) PRIMARY KEY,
+    hub_id VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS conditions (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    type VARCHAR,
-    operation VARCHAR,
+    type VARCHAR(50),
+    operation VARCHAR(50),
     value INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS actions (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    type VARCHAR,
+    type VARCHAR(50),
     value INTEGER
 );
 
 CREATE TABLE IF NOT EXISTS scenario_conditions (
     scenario_id BIGINT REFERENCES scenarios(id) ON DELETE CASCADE,
-    sensor_id VARCHAR REFERENCES sensors(id) ON DELETE CASCADE,
+    sensor_id VARCHAR(255) REFERENCES sensors(id) ON DELETE CASCADE,
     condition_id BIGINT REFERENCES conditions(id) ON DELETE CASCADE,
     PRIMARY KEY (scenario_id, sensor_id, condition_id)
 );
 
 CREATE TABLE IF NOT EXISTS scenario_actions (
     scenario_id BIGINT REFERENCES scenarios(id) ON DELETE CASCADE,
-    sensor_id VARCHAR REFERENCES sensors(id) ON DELETE CASCADE,
+    sensor_id VARCHAR(255) REFERENCES sensors(id) ON DELETE CASCADE,
     action_id BIGINT REFERENCES actions(id) ON DELETE CASCADE,
     PRIMARY KEY (scenario_id, sensor_id, action_id)
 );
 
 CREATE OR REPLACE FUNCTION check_hub_id()
-RETURNS TRIGGER AS
-'
+RETURNS TRIGGER AS $$
 BEGIN
     IF (SELECT hub_id FROM scenarios WHERE id = NEW.scenario_id) != (SELECT hub_id FROM sensors WHERE id = NEW.sensor_id) THEN
-        RAISE EXCEPTION ''Hub IDs do not match for scenario_id % and sensor_id %'', NEW.scenario_id, NEW.sensor_id;
+        RAISE EXCEPTION 'Hub IDs do not match for scenario_id % and sensor_id %', NEW.scenario_id, NEW.sensor_id;
     END IF;
     RETURN NEW;
 END;
-'
-LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE TRIGGER tr_bi_scenario_conditions_hub_id_check
 BEFORE INSERT ON scenario_conditions
