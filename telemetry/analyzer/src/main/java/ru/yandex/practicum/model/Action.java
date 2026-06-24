f0a1605b08044898d8536db115823577c86ba26b
Danny <danny.koshik@gmail.com>
2026-06-24 22:45:14 +0300

    fix(model): remove invalid GeneratedValue from embeddable Action and Condition

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/model/Action.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/model/Action.java
@@ -1,40 +1,27 @@
 package ru.yandex.practicum.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.FieldDefaults;
 import ru.yandex.practicum.kafka.telemetry.event.ActionTypeAvro;
 
-@Entity
-@Table(name = "actions")
+@Embeddable
 @Getter
 @Setter
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Action {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    Long id;
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id")
+//    Long id;
 
-    @Column(name = "type", columnDefinition = "VARCHAR(255)")
+    //@Column(name = "type", columnDefinition = "VARCHAR(255)")
     @Enumerated(EnumType.STRING)
     ActionTypeAvro type;
 
-    @Column(name = "action_value")
+    //@Column(name = "action_value")
     Integer value;
 }
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/model/Condition.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/model/Condition.java
@@ -1,45 +1,32 @@
 package ru.yandex.practicum.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AccessLevel;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.FieldDefaults;
 import ru.yandex.practicum.kafka.telemetry.event.ConditionOperationAvro;
 import ru.yandex.practicum.kafka.telemetry.event.ConditionTypeAvro;
 
-@Entity
-@Table(name = "conditions")
+@Embeddable
 @Getter
 @Setter
-@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Condition {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    Long id;
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id")
+//    Long id;
 
-    @Column(name = "type")
+//    @Column(name = "type")
     @Enumerated(EnumType.STRING)
     ConditionTypeAvro type;
 
-    @Column(name = "operation")
+//    @Column(name = "operation")
     @Enumerated(EnumType.STRING)
     ConditionOperationAvro operation;
 
-    @Column(name = "value")
+//    @Column(name = "value")
     Integer value;
 }
