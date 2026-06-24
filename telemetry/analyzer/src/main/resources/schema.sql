daf512727a674729a9e6148c0fb501e4f441c6cd
Danny <danny.koshik@gmail.com>
2026-06-24 17:25:46 +0300

    fix(analyzer): add custom sql separator for triggers

--- telemetry/analyzer/src/main/resources/schema.sql
+++ telemetry/analyzer/src/main/resources/schema.sql
@@ -3,39 +3,39 @@ CREATE TABLE IF NOT EXISTS scenarios (
     hub_id VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     UNIQUE(hub_id, name)
-);
+)^;
 
 CREATE TABLE IF NOT EXISTS sensors (
     id VARCHAR(255) PRIMARY KEY,
     hub_id VARCHAR(255) NOT NULL
-);
+)^;
 
 CREATE TABLE IF NOT EXISTS conditions (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     type VARCHAR(50),
     operation VARCHAR(50),
     value INTEGER
-);
+)^;
 
 CREATE TABLE IF NOT EXISTS actions (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     type VARCHAR(50),
     value INTEGER
-);
+)^;
 
 CREATE TABLE IF NOT EXISTS scenario_conditions (
     scenario_id BIGINT REFERENCES scenarios(id) ON DELETE CASCADE,
     sensor_id VARCHAR(255) REFERENCES sensors(id) ON DELETE CASCADE,
     condition_id BIGINT REFERENCES conditions(id) ON DELETE CASCADE,
     PRIMARY KEY (scenario_id, sensor_id, condition_id)
-);
+)^;
 
 CREATE TABLE IF NOT EXISTS scenario_actions (
     scenario_id BIGINT REFERENCES scenarios(id) ON DELETE CASCADE,
     sensor_id VARCHAR(255) REFERENCES sensors(id) ON DELETE CASCADE,
     action_id BIGINT REFERENCES actions(id) ON DELETE CASCADE,
     PRIMARY KEY (scenario_id, sensor_id, action_id)
-);
+)^;
 
 CREATE OR REPLACE FUNCTION check_hub_id()
 RETURNS TRIGGER AS $$
@@ -45,14 +45,14 @@ BEGIN
     END IF;
     RETURN NEW;
 END;
-$$ LANGUAGE plpgsql;
+$$ LANGUAGE plpgsql^;
 
 CREATE OR REPLACE TRIGGER tr_bi_scenario_conditions_hub_id_check
 BEFORE INSERT ON scenario_conditions
 FOR EACH ROW
-EXECUTE FUNCTION check_hub_id();
+EXECUTE FUNCTION check_hub_id()^;
 
 CREATE OR REPLACE TRIGGER tr_bi_scenario_actions_hub_id_check
 BEFORE INSERT ON scenario_actions
 FOR EACH ROW
-EXECUTE FUNCTION check_hub_id();
+EXECUTE FUNCTION check_hub_id()^;
